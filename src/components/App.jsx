943d1b4d16a58a86c6a7ed7b10e64aae5efbab56
HarshalDevmurari <92778686+HarshalDevmurari@users.noreply.github.com>
2022-07-24 13:33:24 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+
+  setInterval(updateTime);
+
+  const now = new Date().toLocaleTimeString().substring(0,8);
+  const [time, setTime] = useState(now)
+
+  function updateTime(){
+    const newTime = new Date().toLocaleTimeString().substring(0,8);
+    setTime(newTime);
+  };
+
+  function getTime(){
+    console.log(time)
+  }
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{now}</h1>
+      <button onClick={getTime}>Get Time</button>
+      {/* <button onClick={}>Click me!</button> */}
     </div>
   );
 }
